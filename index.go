7755c227fcf69c1dfdd5abd8d3b3ddcc85fe68e4
Daniel <daniel_tzonkov@abv.bg>
2025-04-11 10:44:57 +0300

    up

--- index.go
+++ index.go
@@ -4,5 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Поздравление")
-	fmt.Println("избери вариант")
+	fmt.Println("Меню:")
+
+	fmt.Println("1. Баланс по сметката")
+	fmt.Println("2. Депозит")
+	fmt.Println("3. Теглене")
+	fmt.Println("4. Изход")
+
+	var choice int
+	fmt.Print("Избери вариант: ")
+	fmt.Scan(&choice)
+
+	fmt.Println("Избор:", choice)
 }
