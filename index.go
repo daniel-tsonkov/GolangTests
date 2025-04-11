1a3df0fb8c83d0b6ccbb1fac67c09d0383eb96c1
Daniel <daniel_tzonkov@abv.bg>
2025-04-11 10:37:08 +0300

    11042025

--- index.go
+++ index.go
@@ -3,22 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var text string = "aaa"
-	var text1 string = "bbb"
-	// var asd float64
-	// fmt.Scan(&asd)
-
-	// fmt.Printf(`%5f`, asd)
-	myText(text)
-	myText(text1)
-
-}
-
-func printText() {
-	var sss = myText("sd")
-	fmt.Println(sss)
-}
-
-func myText(str string) {
-	return str
+	fmt.Println("Поздравление")
+	fmt.Println("избери вариант")
 }
