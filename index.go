13b0de863216c2a03777aaec3dfc64b81cb73484
Daniel <daniel_tzonkov@abv.bg>
2025-04-12 22:24:13 +0300

    up

--- index.go
+++ index.go
@@ -3,19 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var accauntBalance float64 = 1000
+	// var accauntBalance float64 = 1000
 
 	fmt.Println("Поздравление")
 	fmt.Println("Меню:")
+	for {
+		fmt.Println("1. Баланс по сметката")
+		fmt.Println("2. Депозит")
+		fmt.Println("3. Теглене")
+		fmt.Println("4. Изход")
 
-	fmt.Println("1. Баланс по сметката")
-	fmt.Println("2. Депозит")
-	fmt.Println("3. Теглене")
-	fmt.Println("4. Изход")
+		var choice int
+		fmt.Print("Избери вариант: ")
+		fmt.Scan(&choice)
 
-	var choice int
-	fmt.Print("Избери вариант: ")
-	fmt.Scan(&choice)
+		fmt.Println("Избор:", choice)
 
-	fmt.Println("Избор:", choice)
+		if choice == 4 {
+			return
+		}
+	}
 }
