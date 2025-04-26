759a7c8f963e9a5f2eacc4d1f2c9546833cb089d
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-04-26 21:31:53 +0300

    up

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-
 }
 
-func getUserInput(prompt string) {
+func getNOdeData(string, string) {
+	title, err := getUserInput("enter")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	content, err := getUserInput("enter")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func getUserInput(prompt string) (string, error) {
 	fmt.Println(prompt)
 	var value string
 	fmt.Scanln(&value)
+
+	if value == "" {
+		return "", errors.New("Invalit input")
+	}
+	return value, nil
 }
