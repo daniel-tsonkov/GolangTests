c9312393deafab0a6f269e3f779ed5469fd4d7f3
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-04-26 21:36:13 +0300

    up

--- main.go
+++ main.go
@@ -6,22 +6,30 @@ import (
 )
 
 func main() {
-}
-
-func getNOdeData(string, string) {
-	title, err := getUserInput("enter")
+	tittle, content, err := getNOdeData()
 
 	if err != nil {
 		fmt.Println(err)
 		return
+	}
+}
+
+func getNOdeData() (string, string, error) {
+	title, err := getUserInput("enter")
+
+	if err != nil {
+		fmt.Println(err)
+		return "", "", err
 	}
 
 	content, err := getUserInput("enter")
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		return "", "", err
 	}
+
+	return title, content, nil
 }
 
 func getUserInput(prompt string) (string, error) {
