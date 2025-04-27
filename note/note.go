8673f954b231ee66181054a61a32f07743980809
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-04-27 19:04:05 +0300

    up

--- note/note.go
+++ note/note.go
@@ -3,6 +3,8 @@ package note
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -14,6 +16,12 @@ type Note struct {
 
 func (note Note) Display() {
 	fmt.Printf("Your note titled %v has the following content:\n\n%v", note.title, note.content)
+}
+
+func (note Note) Save() {
+	fileName := strings.ReplaceAll(note.title, " ", "_")
+	fileName = strings.ToLower(fileName)
+	os.WriteFile(fileName)
 }
 
 func New(title, content string) (Note, error) {
