1362fb80668a5c6cf0fdbdc675ffb5d586dba3d8
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-05-02 10:19:19 +0300

    up

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
-type Product struct {
-	title string
-	id    string
-	price float64
-}
+import "fmt"
+
+// type Product struct {
+// 	title string
+// 	id    string
+// 	price float64
+// }
 
 func main() {
+	prices := []float64{}
 
+	updatedPrices := append(prices, 5.99)
+
+	fmt.Println(updatedPrices)
 }
