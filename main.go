bd524e8a80a3f0ce163319d0e9f6fd840afc9f47
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-05-08 11:22:20 +0300

    up

--- main.go
+++ main.go
@@ -5,16 +5,24 @@ import "fmt"
 func main() {
 	number := []int{1, 2, 3, 4}
 
-	doubled := doubleNumbers(&number)
+	doubled := transformNumbers(&number, triple)
 
 	fmt.Println(doubled)
 }
 
-func doubleNumbers(numbers *[]int) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
 	for _, value := range *numbers {
-		dNumbers = append(dNumbers, value*2)
+		dNumbers = append(dNumbers, transform(value))
 	}
 
 	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }
