569a5edf94040bed0efdceff047283db1927c6f8
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-05-13 09:41:15 +0300

    13052025

--- main.go
+++ main.go
@@ -1,49 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
-
-type transformFn func(int) int
+import "fmt"
 
 func main() {
-	number := []int{1, 2, 3, 4}
+	result := multiply(5)
 
-	double := createTransformer(2)
-
-	transformed := transformNumbers(&number, func(number int) int {
-		return number * 2
-	})
-
-	doubled := transformNumbers(&number, double)
-
-	fmt.Println(transformed)
-	fmt.Println(doubled)
+	fmt.Println(result)
 }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
-	dNumbers := []int{}
-	for _, value := range *numbers {
-		dNumbers = append(dNumbers, transform(value))
-	}
-
-	return dNumbers
-}
-
-func getTranformenFunction() transformFn {
-	return double
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
-}
-
-func createTransformer(factor int) func(int) int {
-	return func(number int) int {
-		return number * factor
-	}
+func multiply(number int) int {
+	return number * multiply(number-1)
 }
