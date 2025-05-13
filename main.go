884134333af9cbdab29e45bb85455b32dc8a113c
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-05-13 09:49:43 +0300

    up

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	result := multiply(5)
 
-	fmt.Println(result)
-}
-
-func multiply(number int) int {
-	if number == 0 {
-		return 1
-	}
-
-	return number * multiply(number-1)
 }
