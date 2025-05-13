99a2f3f85be931b03c74ade83ac715422b309393
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-05-13 09:57:35 +0300

    up

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	numbers := []int{1, 10, 15}
+	sum := sumup(numbers)
+
+	fmt.Println(sum)
+}
+
+func sumup(numbers []int) int {
+	sum := 0
+
+	for _, val := range numbers {
+		sum += val
+	}
+
+	return sum
 }
