8d4970f45cad30086007863e3bc066dfb60911ca
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-05-19 20:27:41 +0300

    up

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,18 +2,8 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func greet(phrase string) {
-	fmt.Println("Hello", phrase)
-}
-
-func slowGreet(phrase string) {
-	time.Sleep(3 * time.Second)
-	fmt.Println("Hello", phrase)
-}
-
 func main() {
-
+	fmt.Println("go...")
 }
