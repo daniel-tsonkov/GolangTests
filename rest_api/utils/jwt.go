fde91f2e13437899775eb1da46430bf423a5d53e
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-05-26 20:05:08 +0300

    up

--- rest_api/utils/jwt.go
+++ rest_api/utils/jwt.go
@@ -19,8 +19,8 @@ func GenerateToken(email string, userId int64) (string, error) {
 	return token.SignedString([]byte(secretKey))
 }
 
-func VerifyToken(token string) {
-	jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 
 		if !ok {
@@ -28,4 +28,25 @@ func VerifyToken(token string) {
 		}
 		return secretKey, nil
 	})
+
+	if err != nil {
+		return errors.New("Could not parse token.")
+	}
+
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return errors.New("Invalid token")
+	}
+
+	// claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+	// if !ok {
+	// 	return errors.New("Invalid token claims")
+	// }
+
+	// email := claims["email"].(string)
+	// userId := claims["userId"].(int64)
+
+	return nil
 }
