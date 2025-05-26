11f89b7d03be0917daef08a0d3ef90d8795ef734
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-05-26 10:40:48 +0300

    up

--- rest_api/models/user.go
+++ rest_api/models/user.go
@@ -1,6 +1,9 @@
 package models
 
-import "example.com/m/v2/db"
+import (
+	"example.com/m/v2/db"
+	"example.com/m/v2/utils"
+)
 
 type User struct {
 	ID       int64
@@ -18,7 +21,13 @@ func (u User) Save() error {
 
 	defer stmt.Close()
 
-	result, err := stmt.Exec(u.Email, u.Password)
+	hashedPassword, err := utils.HashPassword(u.Password)
+
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
 
 	if err != nil {
 		return err
